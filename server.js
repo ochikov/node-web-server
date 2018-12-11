f9424df37eb0e43f9cc9ea47b7300a50811eb5e6
Ognyan Chikov <o.chikov@propy.com>
2018-12-11 19:08:21 +0200

    Initial Commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+
+	log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFileSync('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Error');
+		}
+	});
+	next();
+});
+
+app.use((req, res, next) => {
+	res.render('maintanance.hbs', {
+		pageTitle: 'Maintenance Page'
+	});
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+	response.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeMessage: 'Hello to mi first express steps'
+	});
+});
+
+app.get('/about', (request, response) => {
+	// response.send('<h1>Hello Express</h1>');
+	response.render('about.hbs', {
+		pageTitle: 'About Page'
+	})
+});
+
+
+app.get('/bad', (request, response) => {
+	response.send({
+		errorMessage: 'Unable to fulfil this request'
+	});
+});
+
+app.listen(3000, () => {
+	console.log('Server is up on port 3000');
+});
